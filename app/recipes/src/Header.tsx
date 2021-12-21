c35900ed2cfb7caf9b034201ec76feb8ff1aa282
Scott Kirklin <scott.kirklin@gmail.com>
2021-12-21 14:35:26 -0600

    search covers ingredients and instructions

--- app/recipes/src/Header.tsx
+++ app/recipes/src/Header.tsx
@@ -30,25 +30,37 @@ function Header() {
 
     function handleSearch(event: ChangeEvent<HTMLInputElement>): void {
         function filterFunc(value: Recipe): boolean {
-            if (value!.name!.toString().toLowerCase().match(event.target.value.toLowerCase()) !== null) {
+            let re = new RegExp(event.target.value.toLowerCase())
+            if (value!.name!.toString().toLowerCase().match(re) !== null) {
                 return true
             }
-            return false
-        }
-        let searchResult = recipes.filter(filterFunc)
-        dispatch({ type: "SET_SEARCH_RESULT", searchResult })
+
+            let matches = Array.prototype.filter.call(value.recipeIngredient, ri => ri.toString().toLowerCase().match(re))
+            if (matches.length > 0) {
+                return true
+            }
+
+            matches = Array.prototype.filter.call(value.recipeInstructions, ri => ri.text!.toString().toLowerCase().match(re))
+            if (matches.length > 0) {
+                return true
+            }
+
+        return false
     }
-    let title = <Title>Recipe box</Title>
-    return (
-        <Container>
-            {title}
-            <Upload directory multiple customRequest={dummyRequest} showUploadList={false}>
-                <Button style={{ float: "right", display: "inline-block" }} icon={<UploadOutlined />}>Upload Directory</Button>
-            </Upload>
-            <label>Search: </label>
-            <input type="text" onChange={handleSearch} />
-        </Container >
-    );
+    let searchResult = recipes.filter(filterFunc)
+    dispatch({ type: "SET_SEARCH_RESULT", searchResult })
+}
+let title = <Title>Recipe box</Title>
+return (
+    <Container>
+        {title}
+        <Upload directory multiple customRequest={dummyRequest} showUploadList={false}>
+            <Button style={{ float: "right", display: "inline-block" }} icon={<UploadOutlined />}>Upload Directory</Button>
+        </Upload>
+        <label>Search: </label>
+        <input type="text" onChange={handleSearch} />
+    </Container >
+);
 }
 
 export default Header;
